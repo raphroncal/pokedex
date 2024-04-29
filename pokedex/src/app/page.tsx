72a963fd2ae8f929be121115bfc57f6be5459a31
Raphael Roncal <raph.roncal01@gmail.com>
2024-04-29 20:24:55 +0800

    fetch api

--- pokedex/src/app/page.tsx
+++ pokedex/src/app/page.tsx
@@ -1,9 +1,49 @@
-'use client';
+"use client";
 
+import { useEffect, useState } from "react";
 import { Card } from "@/components/Card";
+import { PokemonProps } from "@/components/Card";
 
 export default function Home() {
-  return (
-    <Card id={257} name="Blaziken"></Card>
-  )
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchDataForPosts = async () => {
+            try {
+                const response = await fetch(
+                    `https://pokeapi.co/api/v2/pokemon/?limit=1010&offset=0`
+                );
+                if (!response.ok) {
+                    throw new Error(`HTTP error: Status ${response.status}`);
+                }
+                let postsData = await response.json();
+                console.log(postsData.results);
+                setData(postsData.results);
+                setError(null);
+            } catch (err: any) {
+                setError(err.message);
+                setData([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDataForPosts();
+    }, []);
+
+    return (
+        <div>
+            {data &&
+                data.map((pokemon: PokemonProps, index) => (
+                    <Card
+                        id={index + 1}
+                        name={pokemon.name}
+                        url={pokemon.url}
+                        key={index}
+                    ></Card>
+                ))}
+        </div>
+    );
 }
--- pokedex/src/components/Card.tsx
+++ pokedex/src/components/Card.tsx
@@ -1,42 +1,75 @@
+import { useState, useEffect } from "react";
 import { PokemonType } from "./PokemonType";
 
-interface Stat {
-
+export interface Object {
+    name: string;
+    url: string;
 }
 
-interface Type {
-
+export interface Type {
+    slot: number;
+    type: Object;
 }
 
 export interface PokemonProps {
     id: number;
     name: string;
-    // stats: Stat[];
-    // types: Type[];
+    stats?: number[];
+    types?: Type[];
+    url: string;
 }
 
-export const Card = ({
-    id,
-    name,
-    // stats,
-    // types
-}: PokemonProps) => {
+export const Card = ({ id, name, url }: PokemonProps) => {
+    // const types = ["fire", "fighting"];
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const types = ["fire", "fighting"];
+    const paddedId = id.toString().padStart(3, "0");
+
+    useEffect(() => {
+        const fetchDataForPosts = async () => {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`HTTP error: Status ${response.status}`);
+                }
+                let postsData = await response.json();
+                setData(postsData);
+                setError(null);
+            } catch (err: any) {
+                setError(err.message);
+                setData([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDataForPosts();
+    }, []);
+
     return (
         <div className="flex flex-col h-[28rem] w-80 p-8 rounded-3xl bg-slate-900">
             <div className="flex justify-center items-center size-full">
-                <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`} alt="pokemon" className="size-56"/>
+                <img
+                    src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedId}.png`}
+                    alt="pokemon"
+                    className="size-56"
+                />
             </div>
             <div className="flex flex-col gap-4">
                 <div>
                     <p className="font-semibold text-3xl">{name}</p>
-                    <p className="text-slate-500">#{id}</p>
+                    <p className="text-slate-500">#{paddedId}</p>
                 </div>
                 <div className="flex justify-end gap-2">
-                    {types.map((type) => 
-                        <PokemonType type={type}></PokemonType>
-                    )}
+                    {data &&
+                        data?.types?.map((pokemon: Type, index: number) => (
+                            <PokemonType
+                                type={pokemon.type.name}
+                                key={index}
+                            ></PokemonType>
+                        ))}
                 </div>
             </div>
         </div>
